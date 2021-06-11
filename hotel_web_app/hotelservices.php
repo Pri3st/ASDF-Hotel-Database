927c0dfb0ad3a25dd680385f5f287011f611a890
root <root@kalipri3st.Pri3st>
2021-06-11 11:43:39 +0300

    Added About, Team and Technologies HTML Pages

--- hotel_web_app/hotelservices.php
+++ hotel_web_app/hotelservices.php
@@ -6,36 +6,8 @@
 		<link rel="stylesheet" href="bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="button.css">
 	</head>
-<a href="queriesgeneral.html" class="button">Back</a>
+<a href="index.html" class="button">Back</a>
 <center><h2 style = "color:white;">Service Type</h2></center>
-<form action = "Displayagora.php" method="get">
-<br>
-<center style="color:white;">Service Cost(1-10):
-<input type="text" name="search1" size="48">
-</br>
-
-<br>
-<center style="color:white;">Service Day(1-31):
-<input type="text" name="search2" size="48">
-</br>
-
-<br>
-<center style="color:white;">Service Month(1-12):
-<input type="text" name="search3" size="48">
-</br>
-</center>
-<br>
-
-<center style="color:white;">Service Year(2015-2021): 
-<input type="text" name="search4" size="48">
-<br></br>
-<input type="submit" value="submit">
-<input type="reset" value="Reset">
-</center>
-<br>
-
-</form>
-
 
 <?php
 
@@ -43,7 +15,7 @@ echo 'Service Queries Result:';
 
 include("DBConnection.php");
 
-$sql = "SELECT * from hotel.service";
+$sql = "SELECT * from Hotel.service";
 $result = mysqli_query($con,$sql);
 
 ?>
